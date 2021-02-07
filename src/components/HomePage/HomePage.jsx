8730fe2b915b1541c3fdf2792662a053d4049d2e
Vitalii <65967741+SolonukhaVitalii@users.noreply.github.com>
2021-02-07 13:56:02 +0200

    DZ4 components

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+//import PropTypes from 'prop-types';
+
+const HomePage = () => {
+    return (
+        <h1>
+         Домашняя страница   
+        </h1>
+    );
+};
+
+HomePage.propTypes = {
+    
+};
+
+export default HomePage;
