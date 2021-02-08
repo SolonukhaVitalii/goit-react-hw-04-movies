ab28f2ac22e512395e6d2bef43f0308e8a2da978
Vitalii <65967741+SolonukhaVitalii@users.noreply.github.com>
2021-02-08 22:09:42 +0200

    DZ4 lazy

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,11 +1,18 @@
-import React, { Fragment } from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import MovieDetailsPage from './components/MovieDetailsPage';
-import MoviesPage from './components/MoviesPage';
+//import HomePage from './components/HomePage';
+//import MovieDetailsPage from './components/MovieDetailsPage';
+//import MoviesPage from './components/MoviesPage';
+
+const HomePage = lazy(() =>
+  import('./components/HomePage'));
+const MovieDetailsPage = lazy(() =>
+  import('./components/MovieDetailsPage'));
+const MoviesPage = lazy(() =>
+  import('./components/MoviesPage'));
 
 const Routing = () => (
-  <Fragment>
+  <Suspense fallback={<h1>Загружаем...</h1>}>
     <Switch>
     <Route exact path="/" component={HomePage} />
     <Route
@@ -15,6 +22,6 @@ const Routing = () => (
     />
       <Route exact path="/movies" component={MoviesPage} />
       </Switch>
-  </Fragment>
+  </Suspense>
 );
 export default Routing;
